6db05e9f3b3ed690dc199c725afc37a3ac692aae
unkabogaton <anthonralph2@gmail.com>
2023-04-04 15:07:36 +0800

    Update the Navigation bar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,13 +1,29 @@
 <template>
-  <v-app-bar height="55" class="mb-5">
-    <v-tabs class="mx-10">
-      <v-tab exact><v-img src="../assets/logo.svg" width="100"></v-img> </v-tab>
-      <v-tab exact>
+  <v-app-bar
+    height="55"
+    style="background: rgba(0, 0, 0, 0)"
+    elevation="0"
+    absolute
+  >
+    <v-tabs color="primary" class="ml-10">
+      <router-link to="/"
+        ><v-img src="../assets/logo.svg" width="100" class="ma-3 mr-5"></v-img>
+      </router-link>
+      <v-tab exact router to="#">
         Home
       </v-tab>
+      <v-tab router to="#">
+        My Blogs
+      </v-tab>
+      <v-tab>
+        Login/Register
+      </v-tab>
     </v-tabs>
+    <template v-slot:append>
+      <v-avatar color="surface-variant" size="35" class="mr-5"></v-avatar>
+    </template>
   </v-app-bar>
-  <div class="mt-15"><router-view /></div>
+  <v-container class="mt-15"><router-view /></v-container>
 </template>
 
 <script>
@@ -17,9 +33,5 @@ export default {};
 <style>
 .v-tab {
   text-transform: none !important;
-}
-.v-toolbar {
-  background-color: rgba(0, 0, 0, 0.5);
-  color: #fff;
 }
 </style>
