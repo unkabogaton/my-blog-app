d465e366ab4ecf961c5194cb7cecfa8fcaeda669
unkabogaton <anthonralph2@gmail.com>
2023-04-06 16:33:43 +0800

    Blog View UI

---
+++ src/views/BlogView.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar image="" color="secondary" absolute height="400" flat class="">
+  </v-app-bar>
+  <v-container>
+    <v-chip class="text-caption text-main mb-1" color="secondary" label>
+      Category
+    </v-chip>
+    <div class="text-body-2">
+      <span>Published: </span><span>| Updated:</span>
+    </div>
+    <div class="text-h3">Title</div>
+    <div class="text-h5">subtitle</div>
+    <span class="text-caption">Written by: </span>
+    <div class="text-body-1">Content</div>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
