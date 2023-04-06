daadd8f7378d020471f9a10105dedd0476d3e4ab
unkabogaton <anthonralph2@gmail.com>
2023-04-06 16:33:54 +0800

    Add pages to router

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,18 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
     path: "/",
+    name: "Home",
     component: () => import("@/views/BlogHome.vue")
+  },
+  {
+    path: "/create",
+    name: "Create",
+    component: () => import("@/views/BlogCreate.vue")
+  },
+  {
+    path: "/view",
+    name: "View",
+    component: () => import("@/views/BlogView.vue")
   }
 ];
 
