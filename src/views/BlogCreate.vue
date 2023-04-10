da24736fabdcc0f075685ef97ed077e8a335ec53
unkabogaton <anthonralph2@gmail.com>
2023-04-11 00:56:59 +0800

    Integrate Create blog to backend

--- src/views/BlogCreate.vue
+++ src/views/BlogCreate.vue
@@ -1,73 +1,295 @@
 <template>
-  <v-app-bar
-    :image="blogData.photoLink"
-    color="secondary"
-    absolute
-    height="400"
-    flat
-    class=""
-  >
-    <v-btn variant="elevated" color="main" size="small" class="ml-auto mr-1"
-      >Upload</v-btn
+  <div class="text-center mt-15" v-if="loading.uploadBlog == true">
+    <v-progress-circular
+      indeterminate
+      color="primary"
+      :size="150"
+      :width="10"
+    ></v-progress-circular>
+  </div>
+  <div v-else>
+    <v-app-bar
+      :image="photoInitial"
+      color="secondary"
+      absolute
+      height="400"
+      flat
+      class=""
     >
-    or
-    <v-btn variant="elevated" color="main" size="small" class="mr-auto ml-1"
-      >Insert Link</v-btn
-    >
-  </v-app-bar>
-  <v-container>
-    <v-select></v-select>
-    <v-text-field
-      class="title"
-      label="Title"
-      density="compact"
-      required
-      v-model="blogData.title"
-    ></v-text-field>
-    <v-text-field
-      class="subtitle"
-      label="Subtitle (optional)"
-      density="compact"
-      v-model="blogData.subtitle"
-    ></v-text-field>
-    <QuillEditor
-      contentType="html"
-      theme="snow"
-      v-model:content="blogData.content"
-      placeholder="Content"
+      <div class="mx-auto" v-if="photoInitial == ''">
+        <v-btn
+          variant="elevated"
+          color="main"
+          size="small"
+          class="mr-1"
+          @click.prevent="uploadPhoto"
+          >Upload</v-btn
+        >
+        or
+        <v-btn
+          variant="elevated"
+          color="main"
+          size="small"
+          class="ml-1"
+          @click.prevent="dialog.link = true"
+          >Insert Link</v-btn
+        >
+      </div>
+      <div class="mx-auto" v-else>
+        <v-btn
+          variant="elevated"
+          size="small"
+          color="error"
+          @click="removePhoto"
+          >Remove</v-btn
+        >
+      </div>
+      <template v-slot:extension v-if="photoInitial == ''"
+        ><div class="text-h2 text-white mb-15 mx-auto">
+          Cover Photo: 1280px X 400px
+        </div>
+      </template>
+    </v-app-bar>
+    <input
+      type="file"
+      ref="photoInitialRef"
+      id="photoInitial"
+      accept=".png,.jpg,.jpeg"
+      style="visibility: hidden"
+      @change="fileChange"
     />
-    <v-btn @click="increment">Publish Blog</v-btn>
-    <v-btn router :to="{ name: 'Home' }">Preview Blog</v-btn>
-    >
-  </v-container>
+    <v-dialog v-model="dialog.link" persistent>
+      <v-card class="pa-7 mx-auto" min-width="400"
+        ><div class="text-h6 text-center mb-5">Insert Image Link</div>
+        <v-textarea
+          class=""
+          density="compact"
+          v-model="photoInitialLink"
+        ></v-textarea>
+        <v-card-actions
+          ><v-btn
+            color="primary"
+            class="ml-auto"
+            @click.prevent="
+              photoInitial = photoInitialLink;
+              dialog.link = false;
+            "
+            >Confirm</v-btn
+          ><v-btn color="secondary" @click.stop="dialog.link = false"
+            >Cancel</v-btn
+          ></v-card-actions
+        >
+      </v-card>
+    </v-dialog>
+    <v-container>
+      <v-select
+        :items="categories"
+        density="compact"
+        label="Category"
+        v-model="blogData.categories"
+        multiple
+      ></v-select>
+      <v-text-field
+        class="title"
+        label="Title"
+        density="compact"
+        required
+        v-model="blogData.title"
+      ></v-text-field>
+      <v-text-field
+        class="subtitle"
+        label="Subtitle (optional)"
+        density="compact"
+        v-model="blogData.subtitle"
+      ></v-text-field>
+      <v-select
+        :items="['Me only', 'Me and Others']"
+        density="compact"
+        label="Authorship"
+        v-model="authorship"
+      ></v-select>
+      <div class="mb-4">
+        <v-chip
+          v-for="(author, i) in blogData.author"
+          :key="i"
+          :closable="i != 0"
+          @click:close="
+            blogData.author.slice(i, 1);
+            blogData.authorId.slice(i, 1);
+          "
+          class="mr-2"
+        >
+          <span class="ml-2">{{ author.firstname }} {{ author.surname }}</span>
+          <template v-slot:prepend>
+            <v-avatar color="primary" start>{{
+              author.initials
+            }}</v-avatar></template
+          >
+        </v-chip>
+      </div>
+      <div v-if="authorship == 'Me and Others'">
+        <v-row
+          ><v-col cols="4">
+            <div class="mt-2">
+              Please provide the exact email of additional authors:
+            </div></v-col
+          ><v-col cols="4">
+            <v-text-field
+              placeholder="Email of Additonal Author"
+              density="compact"
+              required
+              v-model.trim="email"
+            ></v-text-field
+          ></v-col>
+          <v-col cols="1">
+            <v-btn
+              variant="flat"
+              color="success"
+              block
+              @click.prevent="addAuthor"
+            >
+              ADD
+            </v-btn></v-col
+          ></v-row
+        >
+      </div>
+      <div>Write the blog content below:</div>
+      <QuillEditor
+        contentType="html"
+        theme="snow"
+        v-model:content="blogData.content"
+        placeholder="Content"
+      />
+      <div class="my-10">
+        <v-btn @click="addBlog" color="primary">Publish Blog</v-btn>
+        <v-btn router :to="{ name: 'Home' }" class="ml-3">Preview Blog</v-btn>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script setup>
 import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
 import { ref, onMounted } from "vue";
+import { db } from "@/firebase";
+import {
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  query,
+  where,
+  getDocs
+} from "firebase/firestore";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 // reactive data
-const initialPhoto = ref("");
 const blogData = ref({
-  author: "",
-  category: "",
+  author: [],
+  authorId: [],
+  categories: [],
   content: "",
-  dateCreated: "",
-  dateUpdated: "",
+  dateCreated: Date.now(),
   photoLink: "",
   status: "for approval",
   subtitle: "",
   title: ""
 });
 
+const loading = ref({
+  uploadBlog: false
+});
+
+const dialog = ref({
+  link: false
+});
+
+const categories = ref([
+  "Technology",
+  "Management",
+  "Archives",
+  "Collection",
+  "Reading",
+  "Professionals"
+]);
+
+const user = ref({});
+const photoInitial = ref("");
+const photoFile = ref(null);
+const photoInitialRef = ref("");
+const photoInitialLink = ref("");
+const authorship = ref("Me only");
+const email = ref("");
+
 // display
-onMounted(() => {
-  console.log(blogData.value);
+onMounted(async () => {
+  const uid = sessionStorage.getItem("uid");
+  const docRef = doc(db, "users", uid);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    user.value = docSnap.data();
+  } else {
+    alert("No such document!");
+  }
+  if (authorship.value == "Me only") {
+    blogData.value.author = [
+      {
+        firstname: user.value.firstname,
+        surname: user.value.surname,
+        initials: user.value.initials
+      }
+    ];
+    blogData.value.authorId = [uid];
+  }
 });
 
 // functions
-// function uploadBlog() {}
+async function addAuthor() {
+  if (email.value == "") return;
+  const q = query(collection(db, "users"), where("email", "==", email.value));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach(doc => {
+    if (!blogData.value.authorId.includes(doc.id)) {
+      blogData.value.author.push({
+        firstname: doc.data().firstname,
+        surname: doc.data().surname,
+        initials: doc.data().initials
+      });
+      blogData.value.authorId.push(doc.id);
+      alert("Author Added");
+    } else {
+      alert("Author Already Added");
+    }
+  });
+  email.value = "";
+}
+
+function uploadPhoto() {
+  photoInitialRef.value.click();
+}
+
+function fileChange() {
+  photoFile.value = photoInitialRef.value.files[0];
+  photoInitial.value = URL.createObjectURL(photoFile.value);
+}
+
+function removePhoto() {
+  photoFile.value = null;
+  photoInitial.value = "";
+  photoInitialLink.value = "";
+}
+
+async function addBlog() {
+  loading.value.uploadBlog = true;
+  blogData.value.photoLink = photoInitial.value;
+  const blog = doc(collection(db, "blogs"));
+  await setDoc(blog, blogData.value);
+  loading.value.uploadBlog = false;
+  router.push({ name: "MyBlogs" });
+}
 </script>
 
 <style>
