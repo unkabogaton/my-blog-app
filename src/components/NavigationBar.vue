da087cc8bbf68c2a7b89e75c9aaaae21dde5a599
unkabogaton <anthonralph2@gmail.com>
2023-04-10 22:00:31 +0800

    Update NavigationBar.vue

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,24 +1,45 @@
 <template>
   <v-app-bar height="55" elevation="0" absolute>
-    <router-link class="ml-10" to="/"
+    <router-link class="ml-10" :to="{ name: 'Home' }"
       ><v-img src="../assets/logo.svg" width="100" class="ma-3 mr-5"></v-img>
     </router-link>
     <template v-slot:append>
-      <v-tabs color="primary">
-        <v-tab exact router to="/">
+      <v-tabs color="primary" class="mr-7">
+        <v-tab exact router :to="{ name: 'Home' }" v-if="loggedin == true">
           Home
         </v-tab>
-        <v-tab router to="#">
+        <v-tab router :to="{ name: 'MyBlogs' }" v-if="loggedin == true">
           My Blogs
         </v-tab>
-        <v-tab router to="#">
+        <!-- <v-tab router to="#">
           My Faves
-        </v-tab>
-        <v-btn variant="flat" color="secondary" class="mt-2">
+        </v-tab> -->
+        <v-btn
+          variant="flat"
+          color="secondary"
+          class="mt-2"
+          v-if="loggedin == false"
+          :to="{ name: 'Login' }"
+        >
           Login/Signup
         </v-btn>
       </v-tabs>
-      <v-avatar color="surface-variant" size="35" class="mr-5"></v-avatar>
+      <v-btn
+        variant="outlined"
+        color="secondary"
+        class="mt-1 mr-4"
+        size="small"
+        v-if="loggedin == true"
+        @click="logout"
+      >
+        Logout
+      </v-btn>
+      <v-avatar
+        color="surface-variant"
+        size="35"
+        class="mr-5"
+        v-if="loggedin == true"
+      ></v-avatar>
     </template>
   </v-app-bar>
   <v-main>
@@ -26,8 +47,31 @@
   </v-main>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+
+const loggedin = ref(false);
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+
+const auth = getAuth();
+onAuthStateChanged(auth, user => {
+  if (user) {
+    loggedin.value = true;
+    // ...
+  } else {
+    loggedin.value = false;
+  }
+});
+
+function logout() {
+  signOut(auth)
+    .then(() => {
+      alert("Signed out successfully!");
+    })
+    .catch(error => {
+      alert(error);
+    });
+}
 </script>
 
 <style>
