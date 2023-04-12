f3fb5f9dc790f83a207b55b7d4378ed428be9c0f
unkabogaton <anthonralph2@gmail.com>
2023-04-13 03:08:06 +0800

    Home sort function

--- src/views/BlogHome.vue
+++ src/views/BlogHome.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <v-app-bar
-      image="https://images.unsplash.com/photo-1549675584-91f19337af3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+      v-for="blog in featured"
+      :key="blog.id"
+      :image="blog.photoLink"
       absolute
       height="400"
       flat
@@ -10,7 +12,7 @@
       <template v-slot:image>
         <v-img
           class="pa-10 pl-15"
-          lazy-src="https://images.unsplash.com/photo-1549675584-91f19337af3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=1"
+          :lazy-src="blog.photoLink"
           gradient="to top right, rgba(1,50,32,.8), rgba(0,255,0,.01)"
         >
         </v-img>
@@ -24,18 +26,18 @@
         >
           Featured
         </v-card>
-        <div class="text-h5 text-sm-h4 font-weight-bold">
-          Title
-        </div>
-        <div class="text-body-2 text-sm-body-1">
-          A URL shortener and a QR generator
+        <div
+          class="text-h5 text-sm-h4 font-weight-bold"
+          style="max-width: 600px"
+        >
+          {{ blog.title }}
         </div>
         <v-btn variant="elevated" color="primary" size="small" class="mt-3"
           >Read More</v-btn
         >
       </div>
     </v-app-bar>
-    <v-container class="mt-5">
+    <v-container class="mt-5" style="min-height: 140vh">
       <div class="text-h6 mb-4 text-secondary">Insight Topics</div>
       <v-row>
         <v-col v-for="(cat, i) in categories" :key="i" cols="2">
@@ -45,23 +47,33 @@
         >
       </v-row>
       <div class="text-h6 mt-9 mb-2 text-secondary">All Articles</div>
-      <v-chip-group selected-class="primary" v-model="filter" class="mb-2">
-        <v-chip>Latest</v-chip>
-        <v-chip>Oldest</v-chip>
-        <v-chip>Alpabhetical</v-chip></v-chip-group
+      <v-chip-group
+        selected-class="text-primary"
+        filter
+        v-model="selection"
+        class="mb-2"
+        mandatory
       >
+        <v-chip v-for="(fil, i) in filters" :key="i" :value="fil">{{
+          fil.text
+        }}</v-chip>
+      </v-chip-group>
       <v-row dense>
         <v-col v-for="blog in blogs" :key="blog.id" cols="3">
-          <v-card width="260" elevation="0" to="#" class="pb-3">
+          <v-card max-width="260" elevation="0" to="#" class="pb-3 mb-5">
             <v-img :src="blog.photoLink" width="300" height="150">
-              <v-chip variant="flat" color="secondary" label>
+              <v-chip variant="flat" color="secondary" label size="small">
                 {{ blog.categories[0] }}
               </v-chip>
             </v-img>
-            <div class="text-h6 font-weight-medium my-2 title-post">
+            <div class="text-body-1 font-weight-medium mt-2 mb-1 title-post">
               {{ blog.title }}
             </div>
+            <div class="text-caption text-grey mb-2">
+              {{ convetDate(blog.dateCreated) }}
+            </div>
             <div class="text-caption">
+              by:
               <v-chip
                 v-for="(author, i) in blog.author"
                 :key="i"
@@ -79,17 +91,41 @@
           </v-card>
         </v-col>
       </v-row>
+      <div class="text-center my-15" v-if="loading == true">
+        <v-progress-circular
+          indeterminate
+          color="primary"
+          :size="90"
+          :width="10"
+        ></v-progress-circular>
+      </div>
+      <div class="text-center mb-15">
+        <v-btn
+          v-if="hasMore && !loading"
+          @click="loadMore"
+          color="primary"
+          size="small"
+          >Load More</v-btn
+        >
+      </div>
     </v-container>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { ref, watchEffect, onMounted } from "vue";
 import { db } from "@/firebase";
-import { collection, getDocs } from "firebase/firestore";
-import { useCollection } from "vuefire";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+  startAfter
+} from "firebase/firestore";
+// import { useCollection } from "vuefire";
 
-const blogs = useCollection(collection(db, "blogs"));
+// const blogs = useCollection(collection(db, "blogs"));
 
 const categories = ref([
   "Technology",
@@ -99,16 +135,93 @@ const categories = ref([
   "Reading",
   "Professionals"
 ]);
+const selection = ref({
+  text: "Alphabetical",
+  field: "title",
+  sortType: "asc"
+});
+const filters = ref([
+  { text: "Alphabetical", field: "title", sortType: "asc" },
+  { text: "Latest", field: "dateCreated", sortType: "desc" },
+  { text: "Oldest", field: "dateCreated", sortType: "asc" }
+]);
+const blogs = ref([]);
+const loading = ref(false);
+const lastVisible = ref(null);
+const hasMore = ref(true);
+const featured = ref([]);
 
-const filter = ref("");
+watchEffect(async () => {
+  lastVisible.value = null;
+  hasMore.value = true;
+  loading.value = true;
+  blogs.value = [];
+  const q = query(
+    collection(db, "blogs"),
+    orderBy(selection.value.field, selection.value.sortType),
+    limit(12)
+  );
+  const querySnapshot = await getDocs(q);
+  if (querySnapshot.empty) {
+    hasMore.value = false;
+    loading.value = false;
+    return;
+  }
+  lastVisible.value = querySnapshot.docs[querySnapshot.docs.length - 1];
+  console.log(lastVisible.value);
+  blogs.value = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  if (querySnapshot.docs.length < 12) {
+    hasMore.value = false;
+  }
+  loading.value = false;
+});
+
+async function loadMore() {
+  loading.value = true;
+  if (!lastVisible.value) return;
+  const q = query(
+    collection(db, "blogs"),
+    orderBy(selection.value.field, selection.value.sortType),
+    startAfter(lastVisible.value),
+    limit(12)
+  );
+  const querySnapshot = await getDocs(q);
+  if (querySnapshot.empty) {
+    hasMore.value = false;
+    loading.value = false;
+    return;
+  }
+  lastVisible.value = querySnapshot.docs[querySnapshot.docs.length - 1];
+  console.log(querySnapshot.docs.length);
+  blogs.value = [
+    ...blogs.value,
+    ...querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+  ];
+  if (querySnapshot.docs.length < 12) {
+    hasMore.value = false;
+  }
+  loading.value = false;
+}
 
 onMounted(async () => {
-  const querySnapshot = await getDocs(collection(db, "blogs"));
-  querySnapshot.forEach(doc => {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
-  });
+  const q = query(
+    collection(db, "blogs"),
+    orderBy("dateCreated", "desc"),
+    limit(1)
+  );
+  const querySnapshot = await getDocs(q);
+  featured.value = querySnapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
 });
+
+function convetDate(unix) {
+  const date = new Date(unix);
+  return date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+}
+
+// async function sortBlog() {}
 </script>
 
 <style>
